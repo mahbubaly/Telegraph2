62ab7fdd6eeab6a8b9f828c223b6c6fbd1d4cee3
Mahbubaly <mahbubalikhanofficial@gmail.com>
2023-08-11 15:11:36 +0600

    res

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
                 <header className='flex justify-between  lg:mt-[44px] items-center'>
                     <h1 className='text-[#FF0043] text-xl font-bold lg:pl-[146px]'>Telegraph</h1>
                 
-                        <button className='text-[14px] lg:mr-[175px] font-bold  w-[94px] h-[46px] bg-[#FF0043] rounded-[23px] text-[#fff] '>Try Beta</button>
+                    <button className='text-[14px] lg:mr-[175px] 2xl:mr-[420px]   font-bold  w-[94px] h-[46px] bg-[#FF0043] rounded-[23px] text-[#fff] '>Try Beta</button>
                    
 
                 </header>
@@ -26,14 +26,14 @@ const Home = () => {
                             <p className='font-light  text-lg text-[#00194A] mt-5'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.</p>
 
                             <div className='mt-[50px]'>
-                                <button className='text-[16px] w-[275px] h-[60px] bg-primary font-bold text-[#FFF] rounded-[30px]'>
+                                <button className='text-[16px] w-[275px] h-[60px] bg-primary font-bold text-[#FFF] rounded-[30px] lg:mb-[164px]'>
                                     Request For Early Access
                                 </button>
                             </div>
 
                         </div>
                         <div className=''>
-                            <img src={img} className=' w-[654.712px] h-[604px] pr-[76.37px]' alt="" />
+                            <img src={img} className=' lg:w-[654.712px] h-[604px] pr-[76.37px]' alt="" />
 
 
                         </div>
--- src/Components/Shared/Container.jsx
+++ src/Components/Shared/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Container = ({children}) => {
     return (
-        <div className='max-w-[2520px] mx-auto'>
+        <div className='max-w-[4000px]  mx-auto'>
             {children}
             
         </div>
