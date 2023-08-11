ed53f01f0e34689ab52bf18cb52e6176a106d297
Mahbubaly <mahbubalikhanofficial@gmail.com>
2023-08-11 15:56:41 +0600

    text added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -10,17 +10,17 @@ const Home = () => {
             <Container>
 
 
-                <header className='flex justify-between  lg:mt-[44px] items-center'>
-                    <h1 className='text-[#FF0043] text-xl font-bold lg:pl-[146px]'>Telegraph</h1>
+                <header className='flex justify-between  xl:mt-[44px] items-center'>
+                    <h1 className='text-[#FF0043] text-xl font-bold xl:pl-[146px]'>Telegraph</h1>
                 
-                    <button className='text-[14px] lg:mr-[175px] 2xl:mr-[420px]   font-bold  w-[94px] h-[46px] bg-[#FF0043] rounded-[23px] text-[#fff] '>Try Beta</button>
+                    <button className='text-[14px] xl:mr-[154px]  2xl:mr-[350px]   font-bold  w-[94px] h-[46px] bg-[#FF0043] rounded-[23px] text-[#fff] '>Try Beta</button>
                    
 
                 </header>
 
                 <section>
                     <div className='grid grid-cols-1 lg:grid-cols-2 justify-center gap-[46px] mt-[76px]  items-center'>
-                        <div className='lg:ml-[146px]  '>
+                        <div className=' lg:ml-[146px]  '>
                             <h1 className=' text-6xl leading-[85px] font-medium text-[#00194A]'>Simple. Secure. Reliable messaging.</h1>
 
                             <p className='font-light  text-lg text-[#00194A] mt-5'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.</p>
@@ -32,8 +32,8 @@ const Home = () => {
                             </div>
 
                         </div>
-                        <div className=''>
-                            <img src={img} className=' lg:w-[654.712px] h-[604px] pr-[76.37px]' alt="" />
+                        <div className='2xl:flex justify-center'>
+                            <img src={img} className=' lg:w-[654.712px] h-[604px] pr-[65.29px]' alt="" />
 
 
                         </div>
