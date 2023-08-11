d04caed71c43a68af6c506b052faa3eb12790e07
Mahbubaly <mahbubalikhanofficial@gmail.com>
2023-08-12 01:03:50 +0600

    mobile and dekstop responsive

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -24,28 +24,28 @@ const Home = () => {
                 <nav className='flex justify-between  lg:mt-[44px] items-center'>
                     <h1 className='text-[#FF0043] logoIcon text-xl font-bold lg:pl-[146px]'>Telegraph</h1>
 
-                    <button className='text-[14px] lg:mr-[154px] logoIcon     font-bold  w-[94px] h-[46px] bg-[#FF0043] rounded-[23px] text-[#fff] '>Try Beta</button>
+                    <button className='text-[14px] lg:mr-[150px] logoIcon     font-bold  w-[94px] h-[46px] bg-[#FF0043] rounded-[23px] text-[#fff]  hover:bg-[#D40138] hover:text-[#FFFFFF]'>Try Beta</button>
 
 
                 </nav>
 
                 <header>
-                    <div className='grid grid-cols-1 lg:grid-cols-2 justify-center gap-[46px] mt-[76px]  items-center'>
-                        <div className=' lg:ml-[146px] '>
-                            <h1 className='headwind lg:text-6xl text-[30px] leading-[85px] font-medium text-[#00194A]'>Simple. Secure. Reliable messaging.
+                    <div className='grid grid-cols-1 lg:grid-cols-2 justify-center gap-[46px] mt-[30px] lg:mt-[76px]  items-center'>
+                        <div className=' lg:ml-[150px] '>
+                            <h1 className='headwind lg:text-6xl  text-[30px] lg:leading-[85px] font-medium text-[#00194A]'>Simple. Secure. Reliable messaging.
                             </h1>
 
                             <p className='headerPara mt-5'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.</p>
 
-                            <div className='mt-[50px]'>
-                                <button className='text-[16px] logoIcon w-full lg:w-[275px] h-[60px] bg-primary font-bold text-[#FFF] rounded-[30px] lg:mb-[164px]'>
+                            <div className='mt-[30px] lg:mt-[50px]'>
+                                <button className=' text-[16px] logoIcon w-full lg:w-[275px] h-[60px] bg-primary font-bold text-[#FFF] rounded-[30px] lg:mb-[164px] hover:bg-[#D40138] hover:text-[#FFFFFF]'>
                                     Request For Early Access
                                 </button>
                             </div>
 
                         </div>
-                        <div className='2xl:flex justify-end  2xl:'>
-                            <img src={img} className='w-[280px] mx-auto lg:mx-0  h-[269px] lg:w-[654.712px] lg:h-[604px] lg:pr-[65.29px]' alt="" />
+                        <div className='2xl:flex justify-end '>
+                            <img src={img} className='w-[280px] h-[269px] mx-auto lg:mx-0   lg:w-[654.712px] lg:h-[604px] lg:pr-[65.29px]' alt="" />
 
 
                         </div>
@@ -56,16 +56,16 @@ const Home = () => {
 
                 {/* Card */}
                 <section>
-                    <div className='grid md:grid-cols-3 mt-[67px]  lg:px-[150px] mb-[38px]   grid-cols-1 gap-[30px]    lg:mt-10'>
+                    <div className='grid md:grid-cols-3 mt-[50px] lg:mt-[67px]  lg:px-[150px] mb-[38px]   grid-cols-1 gap-[30px]    '>
 
-                        <div className="card w-[360px] h-[272px] pb-[30px] px-[30px] pt-[36px] bg-[#FFF] CardShadow">
+                        <div className="card w-[280px] mx-auto lg:mx-0  lg:w-[360px] lg:h-[272px] lg:pb-[30px] lg:px-[30px] px-5 py-5 lg:pt-[36px] bg-[#FFF] CardShadow">
 
 
-                            <img src={android} className='w-[39.922px] h-[47.918px] mb-[26px] text-primary' alt="" />
-                            <h1 className='text-[22px] headwind font-medium '>Android</h1>
-                            <p className='headerPara mt-[15px] mb-[21px]'>Lorem Ipsum is simply dummy text of the printing and typesetting.</p>
+                            <img src={android} className='w-[39.922px] h-[47.918px] mb-5 lg:mb-[26px] ' alt="" />
+                            <h1 className='lg:text-[22px] text-5 headwind font-medium mb-[15px] '>Android</h1>
+                            <p className='headerPara lg:text-[16px] text-[14px] mb-5 lg:mt-[15px] space-y-5 lg:mb-[21px]'>Lorem Ipsum is simply dummy text of the printing and typesetting.</p>
 
-                            <div className="flex justify-between items-center">
+                            <div className="flex justify-between items-center ">
                                 <p className='  Try_link text-slate-300 text-[16px] underline font-normal hover:text-secondary '>
                                     Try Beta For Free
                                 </p>
@@ -77,14 +77,17 @@ const Home = () => {
 
 
 
-                        <div className="card w-[360px] h-[272px] pb-[30px] px-[30px] pt-[36px] bg-[#FFF] CardShadow">
+
+                        {/* 2 */}
+
+                        <div className="card w-[280px] mx-auto lg:mx-0  lg:w-[360px] lg:h-[272px] lg:pb-[30px] lg:px-[30px] px-5 py-5 lg:pt-[36px] bg-[#FFF] CardShadow">
 
 
-                            <img src={apple} alt="" className='w-[38.551px] h-[47.567px] mb-[26px] text-primary' />
-                            <h1 className='text-[22px] headwind font-medium '>iPhone</h1>
-                            <p className='headerPara mt-[15px] mb-[21px]'>Lorem Ipsum is simply dummy text of the printing and typesetting.</p>
+                            <img src={apple} className='w-[34px] lg:w-[38.551px] h-[47.567px] mb-[26px]  ' alt="" />
+                            <h1 className='lg:text-[22px] text-5 headwind font-medium mb-[15px] '>iPhone</h1>
+                            <p className='headerPara lg:text-[16px] text-[14px] mb-5 lg:mt-[15px] space-y-5 lg:mb-[21px]'>Lorem Ipsum is simply dummy text of the printing and typesetting.</p>
 
-                            <div className="flex justify-between items-center">
+                            <div className="flex justify-between items-center ">
                                 <p className='  Try_link text-slate-300 text-[16px] underline font-normal hover:text-secondary '>
                                     Try Beta For Free
                                 </p>
@@ -94,13 +97,17 @@ const Home = () => {
 
                         </div>
 
-                        <div className="card w-[360px] h-[272px] pb-[30px] px-[30px] pt-[36px] bg-[#FFF] CardShadow">
+                        {/* 3 */}
 
-                            <img src={macOrWindows} className='w-[110px] mb-[26px]  h-[42px]' alt="" />
-                            <h1 className='text-[22px] headwind font-medium '>Mac or Windows</h1>
-                            <p className='headerPara mt-[15px] mb-[21px]'>Lorem Ipsum is simply dummy text of the printing and typesetting.</p>
 
-                            <div className="flex justify-between items-center">
+                        <div className="card w-[280px] mx-auto lg:mx-0  lg:w-[360px] lg:h-[272px] lg:pb-[30px] lg:px-[30px] px-5 py-5 lg:pt-[36px] bg-[#FFF] CardShadow">
+
+
+                            <img src={macOrWindows} className='lg:w-[101.229px] w-[88px]  lg:h-[47.567px] mb-[26px]  ' alt="" />
+                            <h1 className='lg:text-[22px] text-5 headwind font-medium mb-[15px] '>Mac or Windows</h1>
+                            <p className='headerPara lg:text-[16px] text-[14px] mb-5 lg:mt-[15px] space-y-5 lg:mb-[21px]'>Lorem Ipsum is simply dummy text of the printing and typesetting.</p>
+
+                            <div className="flex justify-between items-center ">
                                 <p className='  Try_link text-slate-300 text-[16px] underline font-normal hover:text-secondary '>
                                     Try Beta For Free
                                 </p>
@@ -109,6 +116,12 @@ const Home = () => {
                             </div>
 
                         </div>
+
+
+
+
+
+
 
 
 
@@ -135,7 +148,7 @@ const Home = () => {
 
                         </div>
                         <div className='lg:ml-[52px]'>
-                            <h1 className='text-[35px] headwind font-medium text-[#00194A] lg:mb-[30px]'>Simple, Personal, Real Time Messaging</h1>
+                            <h1 className='lg:text-[35px] text-[25px] mb-5   headwind font-medium text-[#00194A] lg:mb-[30px]'>Simple, Personal, Real Time Messaging</h1>
                             <p className='text-lg font-light headerPara'>
                                 There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.
 
@@ -156,10 +169,10 @@ const Home = () => {
                 {/* Sharing Cool Stuff With Your Community */}
 
                 <section>
-                    <div className='grid md:grid-cols-2 lg:px-[150px] items-center mt-[50px] lg:mt-[150px] gap-[111px]'>
+                    <div className='grid md:grid-cols-2 lg:px-[150px] items-center mt-[50px] lg:mt-[150px] gap-[30px] lg:gap-[111px]'>
                         <div>
                             <div className=''>
-                                <h1 className='text-[35px] headwind font-medium text-[#00194A] mb-[30px]'>Sharing Cool Stuff With Your Community</h1>
+                                <h1 className='lg:text-[35px] text-[25px] headwind font-medium text-[#00194A] mb-5 lg:mb-[30px]'>Sharing Cool Stuff With Your Community</h1>
                                 <p className='text-lg font-light headerPara'>
                                     There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.
 
@@ -173,10 +186,12 @@ const Home = () => {
                             </div>
 
                         </div>
+
+
                         <div className='lg:flex gap-[30px] items-center'>
 
                             <div>
-                                <div className="pt-[37.4px] px-5 pb-[30px] lg:w-[260px] h-[249px] bg-base-100 shadow-xl CardShadow">
+                                <div className=" pt-[37.4px] px-5 pb-[30px] lg:w-[260px] h-[249px] bg-base-100 shadow-xl CardShadow">
                                     <div className="">
                                         <img src={Security} className='w-[41.918px] h-[47.909px]' alt="" />
 
@@ -216,15 +231,17 @@ const Home = () => {
                 {/* Keep In Touch With The Groups Of People. */}
 
                 <section>
-                    <div className='grid lg:grid-cols-2 gap-[64px] items-center lg:px-[150px]'>
+
+                    <div className='grid lg:grid-cols-2 lg:mt-[80px] mt-[50px] grid-cols-1 gap-[30px] lg:gap-[64px] items-center lg:px-[150px]'>
                         <div className='lg:w-[600px] lg:h-[600px] lg:mx-0 mx-auto w-[280px] h-[357px]'>
                             <img src={womenGroup} className='' alt="" />
 
                         </div>
+
                         <div>
 
                             <div className=''>
-                                <h1 className='text-[35px] headwind font-medium text-[#00194A] mb-[30px]'>Keep In Touch With The Groups Of People.</h1>
+                                <h1 className='lg:text-[35px] text-[25px] headwind font-medium text-[#00194A] mb-[30px]'>Keep In Touch With The Groups Of People.</h1>
                                 <p className='text-lg font-light headerPara'>
                                     There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.
 
@@ -238,34 +255,37 @@ const Home = () => {
                             </div>
                         </div>
                     </div>
+
                 </section>
 
 
 
                 <section>
 
-                    <div
-                        className="bg-[#00194A] p-5 lg:p-0 text-white rounded-[25px] mt-[50px] lg:mt-[179px] lg:mx-[150px] lg:py-[50px]   lg:items-center" >
+                    <div className="bg-[#00194A] pt-[30px] px-5  mx-auto   lg:p-0 lg:w-[1139px] w-[280px] lg:h-[346px] text-white rounded-[25px] mt-[50px] lg:mt-[179px] lg:mx-[150px] lg:py-[50px]   lg:items-center" >
+
+
                         <div className=" text-center">
                             <h1 className=" lg:text-[35px] font-medium headwind">
                                 Let’s Start With Quick App
 
                             </h1>
 
-                            <p className="mx-auto  max-w-[580px] text-[#FFF]  mt-4 headerPara">
+                            <p className="mx-auto mt-5 lg:mb-0 mb-[50px]  lg:max-w-[580px] text-[#FFF]  lg:mt-4 headerPara">
                                 It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
                             </p>
 
-                            <div className="mt-8 flex flex-wrap lg:w-[275px] mx-auto w-full   justify-center gap-4">
-                                <a
-                                    className="block w-full logoIcon font-bold rounded-[30px] border  bg-primary px-12 py-3 text-sm  text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
-                                    href="/get-started"
-                                >
+
+                           
+
+
+                            <div className='mt-[30px]  lg:mb-0 lg:mt-[50px]'>
+                                <button className='mb-[30px] text-[16px] logoIcon w-full lg:w-[275px] h-[60px] bg-primary font-bold text-[#FFF] rounded-[30px] lg:mb-[164px]  hover:bg-[#D40138] hover:text-[#FFFFFF]'>
                                     Request For Early Access
-                                </a>
-
-
+                                </button>
                             </div>
+
+
                         </div>
                     </div>
                 </section>
@@ -275,11 +295,11 @@ const Home = () => {
 
 
 
-                <div className='md:flex lg:px-[150px] lg:my-[80px] my-[50px] justify-between p-2 text-[#00194A]'>
+                <div className='md:flex lg:px-[150px]  lg:my-[80px] my-[50px] justify-between p-2 text-[#00194A]'>
                     <div>
-                        <p className='text-lg headwind font-medium'>© Telegraph. 2020 — All rights reserved.</p>
+                        <p className='lg:text-lg text-[14px]  headwind font-medium'>© Telegraph. 2020 — All rights reserved.</p>
                     </div>
-                    <div className='flex gap-6 text-[16px] headwind font-medium lg:justify-start justify-center text-[#00194A]'>
+                    <div className='flex lg:gap-4 gap-2 text-[16px] px-5 lg:px-0 mt-5 lg:mt-0 headwind font-medium  justify-center  text-[#00194A]'>
                         <p>Twitter</p>
                         <p>Instagram</p>
                         <p>Facebook</p>
--- src/Components/Shared/Container.jsx
+++ src/Components/Shared/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Container = ({children}) => {
     return (
-        <div className='max-w-[4000px] lg:p-0 p-5 mx-auto'>
+        <div className='max-w-[4000px] lg:py-0 lg:px-0 px-5 py-[30px] mx-auto'>
             {children}
             
         </div>
